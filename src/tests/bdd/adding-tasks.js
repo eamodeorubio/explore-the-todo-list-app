b679f517b2d567cfe9d98ee31ce9f97dd5f2dd0a
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-07-18 17:26:30 +0200

    Refactored shared specs

--- src/tests/bdd/adding-tasks.js
+++ src/tests/bdd/adding-tasks.js
@@ -1,3 +1,31 @@
+function describeAddingANewTask(mainPage, newTaskDescription) {
+  var expectedNewTask;
+
+  beforeEach(function () {
+    expectedNewTask = {
+      text:newTaskDescription,
+      done:false
+    };
+  });
+
+  it("will see that the new task is being created", function () {
+    expectedNewTask.inProgress = true;
+    expect(mainPage().displayedTasks()).toEqual([expectedNewTask]);
+  });
+
+  it("will see that the new task is created after a short period of time", function () {
+    waitsFor(function () {
+      var tasks = mainPage().displayedTasks();
+      return tasks.length === 1 && tasks[0].inProgress === false;
+    }, "Task is not created or operation is too slow", 1000);
+
+    runs(function () {
+      expectedNewTask.inProgress = false;
+      expect(mainPage().displayedTasks()).toEqual([expectedNewTask]);
+    })
+  });
+}
+
 describe("The todo list apps allows to add new tasks", function () {
   describe("Given the application has been started and there are no tasks yet", function () {
     var mainPage, started;
@@ -38,56 +66,28 @@ describe("The todo list apps allows to add new tasks", function () {
       expect(started).toBe(true);
     });
 
+    function getMainPage() {
+      return mainPage;
+    }
+
     describe("Given the user has specified the new task description as 'Hola'", function () {
-      var newTaskDescription = 'Hola', expectedNewTask;
+      var newTaskDescription = 'Hola';
       beforeEach(function () {
-        expectedNewTask = {
-          text:newTaskDescription,
-          done:false
-        };
         mainPage.fillNewTaskDescription(newTaskDescription);
       });
       describe("When the user request the task to be added using the keyboard", function () {
         beforeEach(function () {
           mainPage.requestNewTaskUsingKeyboard();
         });
-        it("will see that the new task is being created", function () {
-          expectedNewTask.inProgress = true;
-          expect(mainPage.displayedTasks()).toEqual([expectedNewTask]);
-        });
-        it("will see that the new task is created after a short period of time", function () {
 
-          waitsFor(function () {
-            var tasks = mainPage.displayedTasks();
-            return tasks.length === 1 && tasks[0].inProgress === false;
-          }, "Task is not created or operation is too slow", 1000);
-
-          runs(function () {
-            expectedNewTask.inProgress = false;
-            expect(mainPage.displayedTasks()).toEqual([expectedNewTask]);
-          })
-        });
+        describeAddingANewTask(getMainPage, newTaskDescription);
       });
       describe("When the user request the task to be added without using the keyboard", function () {
         beforeEach(function () {
           mainPage.requestNewTaskWithoutKeyboard();
         });
-        it("will see that the new task is being created", function () {
-          expectedNewTask.inProgress = true;
-          expect(mainPage.displayedTasks()).toEqual([expectedNewTask]);
-        });
-        it("will see that the new task is created after a short period of time", function () {
 
-          waitsFor(function () {
-            var tasks = mainPage.displayedTasks();
-            return tasks.length === 1 && tasks[0].inProgress === false;
-          }, "Task is not created or operation is too slow", 1000);
-
-          runs(function () {
-            expectedNewTask.inProgress = false;
-            expect(mainPage.displayedTasks()).toEqual([expectedNewTask]);
-          })
-        });
+        describeAddingANewTask(getMainPage, newTaskDescription);
       });
     });
   });
