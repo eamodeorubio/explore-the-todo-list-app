59036c71b08e0da4d2fb5e0f278d6262d9dffdce
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-07-19 20:35:53 +0200

    Fixed page objects to work with PhantomJS 1.5 (Travis CI works with that version). A lot of nasty fixes and workaroud to emulate some 1.6 version features in PhantomJS 1.5

--- src/tests/utils/page-objects.js
+++ src/tests/utils/page-objects.js
@@ -1,8 +1,36 @@
 var test = (function (ns, browser) {
   ns = ns || {};
 
+  function fixPhantom15EvaluateMethodToAddParameterPassing(webPage) {
+    if (phantom.version.major <= 1 && phantom.version.minor <= 5) {
+      //var oldEvaluate = webPage.evaluate;
+      webPage.executeCommand = function (func, args) {
+        // Thanks god the code is open source. Adapted from: https://github.com/ariya/phantomjs/commit/81794f9096
+        var str, arg, i, l;
+        if (!(func instanceof Function || typeof func === 'string' || func instanceof String)) {
+          throw "Wrong use of WebPage#evaluate";
+        }
+        str = 'function() { return (' + func.toString() + ')(';
+        for (i = 1, l = arguments.length; i < l; i++) {
+          arg = arguments[i];
+          if (/object|string/.test(typeof arg) && !(arg instanceof RegExp)) {
+            str += 'JSON.parse(' + JSON.stringify(JSON.stringify(arg)) + '),';
+          } else {
+            str += arg + ',';
+          }
+        }
+        str = str.replace(/,$/, '') + '); }';
+        return webPage.evaluate(str);
+      };
+    } else
+      webPage.executeCommand = webPage.evaluate;
+    return webPage;
+  }
+
   function MainPage(webPage) {
     var self = this, pendingCallbacks = {};
+
+    webPage = fixPhantom15EvaluateMethodToAddParameterPassing(webPage);
 
     function randomId() {
       return Math.round(Math.random() * 1e20) + "";
@@ -29,11 +57,23 @@ var test = (function (ns, browser) {
     };
 
     webPage.onConsoleMessage = function (msg) {
-      console.log(msg);
+      var callbacksignal = '####[oncallback]####', params;
+      if (msg.indexOf(callbacksignal) === 0) {
+        // Fixing that phantom 1.5 does not have onCallback
+        params = JSON.parse(msg.substring(callbacksignal.length));
+        webPage.onCallback.apply(this, params);
+      } else
+        console.log(msg);
     };
 
     webPage.onInitialized = function () {
-      webPage.evaluate(function () {
+      webPage.executeCommand(function () {
+        if (typeof window.callPhantom !== 'function') {
+          window.callPhantom = function () {
+            // Fixing that phantom 1.5 does not have onCallback
+            console.log('####[oncallback]####' + JSON.stringify(Array.prototype.slice.call(arguments)));
+          };
+        }
         window.underTest = true;
       });
     };
@@ -58,13 +98,13 @@ var test = (function (ns, browser) {
     };
 
     this.emptyTheTaskList = function (callback) {
-      webPage.evaluate(function (returnToPhantom) {
+      webPage.executeCommand(function (returnToPhantom) {
         testStorage.removeAll(returnToPhantom);
       }, returnFromSandboxAndInvokeTheCallback(callback));
     };
 
     this.setupTheTaskList = function (initialTasks, callback) {
-      webPage.evaluate(function (initialTasks, returnToPhantom) {
+      webPage.executeCommand(function (initialTasks, returnToPhantom) {
         var totalToSave = initialTasks.length, errors = 0;
         initialTasks.forEach(function (task) {
           task.description = task.text;
@@ -81,13 +121,13 @@ var test = (function (ns, browser) {
     };
 
     this.startApplication = function (callback) {
-      webPage.evaluate(function (returnToPhantom) {
+      webPage.executeCommand(function (returnToPhantom) {
         window.startApp(returnToPhantom);
       }, returnFromSandboxAndInvokeTheCallback(callback));
     };
 
     this.displayedTasks = function () {
-      var json = webPage.evaluate(function () {
+      var json = webPage.executeCommand(function () {
         var tasks = [];
         $('.task-list > .task').each(function () {
           var el = $(this), task = {};
@@ -102,7 +142,7 @@ var test = (function (ns, browser) {
     };
 
     this.requestToggleTaskUsingCheck = function (taskIndex) {
-      webPage.evaluate(function (taskIndex) {
+      webPage.executeCommand(function (taskIndex) {
         var chk = $('.task-list > .task').eq(taskIndex).find('.chk');
         if ('ko' in window) {
           // knockout needs to change the checked property before the click in order to notice the event
@@ -113,32 +153,32 @@ var test = (function (ns, browser) {
     };
 
     this.requestToggleTaskNotUsingCheck = function (taskIndex) {
-      webPage.evaluate(function (taskIndex) {
+      webPage.executeCommand(function (taskIndex) {
         $('.task-list > .task').eq(taskIndex).find('.txt').trigger('click');
       }, taskIndex);
     };
 
     this.fillNewTaskDescription = function (text) {
-      webPage.evaluate(function (text) {
+      webPage.executeCommand(function (text) {
         $(".add-task-widget > .txt").val(text).trigger('keyup').trigger('change');
       }, text);
     };
 
     this.requestNewTaskUsingKeyboard = function () {
-      webPage.evaluate(function () {
+      webPage.executeCommand(function () {
         var e = $.Event("keyup", {which:13, keyCode:13});
         $(".add-task-widget > .txt").focus().trigger(e);
       });
     };
 
     this.requestNewTaskWithoutKeyboard = function () {
-      webPage.evaluate(function () {
+      webPage.executeCommand(function () {
         $(".add-task-widget > .btn").focus().trigger('click');
       });
     };
 
     this.hasBeenDefined = function (symbol) {
-      return webPage.evaluate(function (symbol) {
+      return webPage.executeCommand(function (symbol) {
         return symbol in window;
       }, symbol);
     };
